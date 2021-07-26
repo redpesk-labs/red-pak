38f057432aee312187fc58f28931c2ce6caa464f
Clément Bénier <clement.benier@iot.bzh>
2021-07-26 14:51:23 +0200

    remove former directories
    Signed-off-by: Clément Bénier <clement.benier@iot.bzh>

--- red-dnf/cli-dnf/rpm__init__.py
+++
@@ -1,6 +0,0 @@
-# make sure to enforce redrpm usage
-import sys
-import redrpm
-
-sys.modules['rpm'] = sys.modules['redrpm']
-print ("**** Map Original LibRPM to RedRPM (should not be needed !!! ) ***")
--- red-rpm/module-py/sources-c/rpmlib-ori/rpmtd-py.h
+++
@@ -1,9 +0,0 @@
-#ifndef H_RPMTD_PY
-#define H_RPMTD_PY
-
-PyObject * rpmtd_ItemAsPyobj(rpmtd td, rpmTagClass tclass);
-PyObject * rpmtd_AsPyobj(rpmtd td);
-
-int rpmtdFromPyObject(PyObject *obj, rpmtd *td);
-
-#endif
