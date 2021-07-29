eaea6c9fbfe37f3a612ff186c20293d91ce69fef
Clément Bénier <clement.benier@iot.bzh>
2021-07-29 17:41:55 +0200

    red-plugin: rpm plugin for red-pak
    Signed-off-by: Clément Bénier <clement.benier@iot.bzh>

---
+++ red-plugin/rpm-redpak.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <libgen.h>
+#include <linux/limits.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <string.h>
+#include <signal.h>
+
+#include <rpm/rpmlog.h>
+#include <rpm/rpmts.h>
+#include <rpm/rpmts.h>
+#include <rpm-plugins/rpmplugin.h>
+
+static rpmRC redpak_scriptlet_pre(rpmPlugin plugin, const char *s_name,
+                                     int type)
+{
+    chroot(".");
+    return RPMRC_OK;
+}
+
+struct rpmPluginHooks_s redpak_hooks = {
+	.scriptlet_pre = redpak_scriptlet_pre,
+};
