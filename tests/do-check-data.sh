42f14ace7fbd9e14baf379ae57fc82f557c0b32b
José Bollo <jose.bollo@iot.bzh>
2024-10-01 16:38:30 +0200

    Improve tests in nodes

--- tests/do-check-data.sh
+++ tests/do-check-data.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 rednode=/tmp/redpak-check-data
-for prog in data/*-prog
+for prog in $(ls data/*-prog 2>/dev/null | sort -t/ -k2n)
 do
     num=$(echo -n "${prog}" | sed 's:.*/\([0-9]*\)-.*:\1:')
     if test -f "data/${num}-normal.yaml"
@@ -18,14 +18,26 @@ do
     fi
     refe="data/${num}-reference"
     resu="data/${num}-result"
-    cmd="$(cat "${prog}" | sed 's/#.*//;/^[ \n]*$/d;s/.*/ & /' | tr '\n' ';')"
     bbox=$(awk -vr=busybox '$1~/mount/&&$2~/busybox/{r=$2}END{print r}' "${normal}")
 
     echo
     echo "====== TEST ${num} ======="
+
+    # create the node
     rm -rf "${rednode}"
     echo "redconf create --alias NODE-${num} --config ${normal} --config-adm ${admin} ${rednode}"
     redconf create --alias "NODE-${num}" --config "${normal}" --config-adm "${admin}" "${rednode}"
+
+    cmd="/testscript/${prog##*/}"
+    if grep -q "${cmd}" "${normal}"
+    then
+        mkdir -p "$(dirname "${rednode}${cmd}")"
+        cp "${prog}" "${rednode}${cmd}"
+        chmod +x "${rednode}${cmd}"
+    else
+        cmd="$(cat "${prog}" | sed 's/#.*//;/^[ \n]*$/d;s/.*/ & /' | tr '\n' ';')"
+    fi
+
     echo "redwrap --redpath ${rednode} -- ${bbox} sh -c ${cmd}"
     redwrap --redpath "${rednode}" -- "${bbox}" sh -c "${cmd}" > "${resu}"
     diff "${refe}" "${resu}"
