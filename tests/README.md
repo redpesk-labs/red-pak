920b00e44446d3ae06c3c296ee63585fbb09d31b
José Bollo <jose.bollo@iot.bzh>
2024-11-19 14:22:09 +0100

    Add tests for config

--- tests/README.md
+++ tests/README.md
@@ -1,3 +1,48 @@
+# Content
+
+This directory contains 2 unitary tests, a set of basic
+tests and a set of live tests.
+
+## check.c
+
+This test needs redmicrodnf.
+
+Requires special setting (see below) when test
+environment isn't standard.
+
+## check-conf.c
+
+This test checks some basic functions of library
+redconf.
+
+Requires special setting (see below) when test
+environment isn't standard.
+
+## directory basic
+
+This directory contains a test script that check
+how rednode stacking affects the arguments passed
+to to bubble wrap launcher *bwrap*.
+
+## directory conf
+
+This directory contains a test script that check
+how if configuration files are correctly parsed.
+
+## directory live
+
+This directory contains a test script and its
+data for checking that running environments as setup
+by redwrap is conforming expectation.
+
+Live tests are requiring the following packages
+to be installed:
+
+- busybox
+- gcc
+- glibc-static
+
+## Setting test environment
 
 In order to run the tests, it might be necessary to export the below variables:
 
---
+++ tests/conf/do-conf-tests.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+tap=true
+test "$1" = "notap" && tap=false
+
+redroot=/tmp/rpak-conftest
+tmp=${redroot}.out
+
+# cleaning
+
+filter='s,\[/[^]i:]*:[0-9]*][ \t]*$,,;s,[ \t]*$,,'
+
+do_test() {
+    local input="$1" output="$2" reference="$3"
+    rm -rf "${redroot}"
+    mkdir -p "${redroot}/etc"
+    cp "${input}" "${redroot}/etc/redpack.yaml"
+    cat << EOC > "${redroot}/.rednode.yaml"
+realpath: ${redroot}
+info: created by do-conf-tests.sh the Wed Jan  1 07:00:00 UTC 2020
+timestamp: 1577862000000
+state: Enable
+EOC
+    echo "redconf --yaml config ${redroot} > ${output}"
+    redconf --yaml config "${redroot}" |&
+      sed "${filter}" >& "${output}"
+    grep -v '\<IGNORE\>' "${output}" |
+      diff -N "${reference}" -
+}
+
+rm -rf "${redroot}" "${tmp}"
+$tap && echo "TAP version 14"
+itap=0
+for num in $(ls data/*-input 2>/dev/null | sed 's:data/::;s:-input::' | sort -n)
+do
+    itap=$((itap + 1))
+    input="data/${num}-input"
+    output="data/${num}-output"
+    reference="data/${num}-reference"
+    title="test-conf-${num}"
+    do_test "${input}" "${output}" "${reference}" >& "${tmp}"
+    sts=$?
+    if $tap; then
+        if test $sts -gt 0; then
+            echo "not ok $itap - ${title}"
+            sed 's:^:   :' "${tmp}"
+        else
+            echo "ok $itap - ${title}"
+        fi
+    else
+        cat "${tmp}"
+    fi
+done
+$tap && echo "1..${itap}"
+echo
